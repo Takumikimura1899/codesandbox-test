2dde8aef34a17d9d7ffd1dd75fb78411ec16851d
Takumikimura1899 <81891074+Takumikimura1899@users.noreply.github.com>
2021-05-22 08:24:55 +0900

    modanJSの機能に触れる

--- src/index.js
+++ src/index.js
@@ -60,7 +60,6 @@
 // console.log(func2("func2です"));
 
 // const func3 = (num1, num2) => num1 + num2;
-
 // console.log(func3(10, 20));
 
 /**
@@ -143,11 +142,52 @@ const nameArr = ["田中", "山田", "拓"];
 // });
 // console.log(newNumArr);
 
-const newNameArr = nameArr.map((name) => {
-  if (name === "拓") {
-    return name;
-  } else {
-    return `${name}さん`;
-  }
-});
-console.log(newNameArr);
+// const newNameArr = nameArr.map((name) => {
+//   if (name === "拓") {
+//     return name;
+//   } else {
+//     return `${name}さん`;
+//   }
+// });
+// console.log(newNameArr);
+
+/**
+ * 三項演算子
+ */
+// ある条件 ? 条件がtrueの時 : 条件がfalseの時
+// const val1 = 1 < 0 ? 'trueです' : 'falseです';
+// console.log(val1);
+
+// const num = 1300;
+// console.log(num.toLocaleString());
+
+// const formattedNum = typeof num === 'number' ? num.toLocaleString() : '数値を入力して下さい'
+// console.log(formattedNum);
+
+// const checkSum = (num1, num2) => {
+//   return num1 + num2 > 100 ? '100を超えています!!' : '許容範囲内です';
+// }
+// console.log(checkSum(50,40));
+
+/**
+ * 論理演算子の本当の意味を知ろう && ||
+ */
+// const flag1 = true;
+// const flag2 = true;
+
+// if(flag1 || flag2) {
+//   console.log("1か2はtrueになります");
+// }
+// if(flag1 && flag2) {
+//   console.log("1も2もtrueになります");
+// }
+
+// ||は左側がfalseなら右側を返す
+// const num = 100;
+// const fee = num || "金額未設定です";
+// console.log(fee);
+
+// &&は左側がtrueなら右側を返す
+// const num2 = 100;
+// const fee2 = num2 && "何か設定されました";
+// console.log(fee2);
